2ec72ca1f4587c936acb9f39d37697b096366257
Gemma Black <gemma@getrentr.com>
2019-06-22 20:20:05 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Duck schema
+
+A schema that extends JSON Schema for use with databases.
+
---
+++ examples/post.js
@@ -0,0 +1,34 @@
+module.exports = {
+    "id": "http://theworldisours/post.json",
+    "$schema": "http://json-schema.org/draft-07/schema#",
+    "description": "Post schema",
+    "type": "object",
+    "title": "post",
+    "properties": {
+        "id": {
+            "type": "int",
+            "maxLength": 11,
+            "autoincrement": true
+        },
+        "title": {
+            "type": "string",
+            "minLength": 1,
+            "maxLength": 80
+        },
+        "subtitle": {
+            "type": "string",
+            "minLength": 1,
+            "maxLength": 144
+        },
+        "article": {
+            "type": "string",
+            "minLength": 1,
+            "maxLength": 999999
+        }
+        
+    },
+    "required": [
+        "id",
+        "title"
+    ]
+}
